43ea4eebd2d60bfb7e7956726ee6cd5fdccbcaec
JeffTunner <abhinavkaushik05@gmail.com>
2025-07-30 19:59:15 +0530

    Day 3- installed tailwindcss, and configured it manually

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -3,6 +3,8 @@ function Dashboard() {
     return (
         <div>
             <h1>Dashboard</h1>
+            <h1 className="text-4xl font-bold text-blue-600">Tailwind is working!</h1>
+
             {/* Add your dashboard content here */}
         </div>
     );
