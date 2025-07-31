382915546efd5a7d23e4fea578c090c4b1446070
JeffTunner <abhinavkaushik05@gmail.com>
2025-07-31 22:25:25 +0530

    Day 4- Styled BaseLayout and also created a functioning Login Page

--- src/components/BaseLayout.jsx
+++ src/components/BaseLayout.jsx
@@ -2,23 +2,23 @@ import { Link, Outlet } from "react-router-dom";
 function BaseLayout() {
 
     return (
-        <div>
-            <header>
-                <h1>Student Productivity Tracker</h1>
-                <p>Your very own time tracking app👌</p>
+        <div className="min-h-screen flex flex-col bg-amber-50">
+            <header className="bg-blue-100 py-4 px-6 text-center flex-col border-2 rounded-xl shadow-2xl m-10">
+                <h1 className="font-extrabold text-3xl text-blue-950 pb-3">Student Productivity Tracker</h1>
+                <p className="font-semibold text-pink-600">Your very own time tracking app👌</p>
             </header>
-            <nav>
-                <ul>
-                    <li><Link to="/dashboard">Dashboard</Link></li>
-                    <li><Link to="/signup">SignUp</Link></li>
-                    <li><Link to="/login">Login</Link></li>
+            <nav className="bg-gray-100 px-6 py-3 shadow-md m-10 mt-0 rounded-lg">
+                <ul className="flex justify-around gap-8 text-sm font-sans">
+                    <li><Link to="/dashboard" className="hover:text-blue-800">Dashboard</Link></li>
+                    <li><Link to="/signup" className="hover:text-emerald-500">SignUp</Link></li>
+                    <li><Link to="/login" className="hover:text-emerald-500">Login</Link></li>
                 </ul>
             </nav>
-            <main>
+            <main className="flex-grow px-6 py-4 justify-center items-center flex">
                 <Outlet />
             </main>
-            <footer>
-                <p>&copy; 2025 Student Productivity Tracker | Abhinav Kaushik </p>
+            <footer className="bg-gray-200 text-center py-4 hover:bg-gray-300">
+                <p className="text-slate-700 font-extralight hover:text-rose-400 cursor-pointer">&copy; 2025 Student Productivity Tracker | Abhinav Kaushik </p>
             </footer>
         </div>
     );
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,10 +1,29 @@
 
 function Login() {
+
     return (
-        <div>
-            <h1 className="bg-red-500">Login Page</h1>
-            {/* Add your login form here */}
+        <div className="min-h-screen flex flex-col items-center justify-center bg-amber-50">
+        <div className="flex flex-col items-center justify-center shadow-md rounded-2xl p-6 mb-5 bg-slate-100 max-[400px]:w-full md:shadow-lg">
+            <div className="text-center mb-4 bg-blue-200 p-4 rounded-lg">
+                <h3 className="font-medium text-rose-600 text-3xl font-sans">Hello😊</h3>
+                <p className="font-thin text-orange-800 font-sans">Please Enter your Credentials to Login!</p>
+            </div>
+            <form onSubmit={(e) => {e.preventDefault(); alert("You have successfully Logged In!!!");}}>
+                <div className="mb-4 flex flex-col">
+                    <label aria-label="Email" htmlFor="email" className="block text-gray-700 mb-1 text-xl font-semibold">Email:</label>
+                    <input autoComplete="email" type="email" className="w-full rounded-md border-gray-200 border px-3 py-2 focus:outline-none focus:ring-2 focus:ring-sky-200" name="Email" id="email" />
+                </div>
+                <div className="mb-8 flex flex-col">
+                    <label aria-label="Password" htmlFor="password" className="block text-gray-700 mb-1 text-xl font-semibold">Password:</label>
+                    <input autoComplete="current-password" type="password" className="w-full rounded-md border-gray-200 border px-3 py-2 focus:outline-none focus:ring-2 focus:ring-sky-200" name="Password" id="password" />
+                </div>
+                <div className="flex justify-center mb-4">
+                    <button className="w-full transition-shadow duration-200 text-slate-50 bg-green-500 px-3 py-2 text-xl font-semibold rounded-md hover:bg-green-400 hover:shadow-md" type="submit">Login</button>
+                </div>
+            </form>
         </div>
+        </div>
+
     );
 }
 export default Login;
