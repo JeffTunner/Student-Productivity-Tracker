4041eb2e0c57768b4a636370a3dbfb4e61804d53
JeffTunner <abhinavkaushik05@gmail.com>
2025-08-06 22:57:46 +0530

    Day 9- Created a TaskCard component with multiple user responses

--- src/components/tracker/DailyView.jsx
+++ src/components/tracker/DailyView.jsx
@@ -1,3 +1,5 @@
+import React, { useState } from "react";
+import TaskCard from "./TaskCard.jsx";
 
 function DailyView({username}) {
 
@@ -19,6 +21,34 @@ function DailyView({username}) {
         day: 'numeric'
     });
 
+    const [taskCard, setTaskCard] = useState([]);
+    const [taskTitle, setTaskTitle] = useState("");
+    const [taskDescription, setTaskDescription] = useState("");
+    const [taskList, setTaskList] = useState([]);
+    const [task, setTask] = useState("");
+
+    function handleAddTaskCard() {
+
+        const newCard = {id: Date().now, title: taskTitle, description: taskDescription, list: []};
+        const newList = {name: task};
+
+        setTaskCard(tc => [...tc, newCard]);
+        setTaskList(tl => [...tl, newList]);
+        setTaskTitle("");
+        setTaskDescription("");
+        setTask("");
+    }
+
+    function handleTaskTitle(event) {
+        setTaskTitle(event.target.value);
+    }
+
+    function handleTaskDescription(event) {
+        setTaskDescription(event.target.value);
+    }
+
+    
+
     return (
     <div>
             <header className="flex flex-col items-center gap-2 bg-gray-100 font-mono p-4 border border-slate-950">
@@ -32,99 +62,37 @@ function DailyView({username}) {
                     <p>Click + to add one!</p>
                 </div>
                 <div className="mt-4">
-                    <button className="bg-slate-800 text-white p-2 font-extrabold rounded-lg shadow-lg hover:bg-slate-700 hover:shadow-2xl hover:shadow-slate-950 hover:scale-105 transform transition-transform duration-300">+ Add Task</button>
+                    <button className="bg-slate-800 text-white p-2 font-extrabold rounded-lg shadow-lg hover:bg-slate-700 hover:shadow-2xl hover:shadow-slate-950 hover:scale-105 transform transition-transform duration-300" onClick={handleAddTaskCard}>+ Add Card</button>
                 </div>
 
                 <div className="flex flex-wrap gap-8">
-                    <div className="flex flex-col border-slate-900 border-2 rounded-3xl px-8 py-6 shadow-xl w-[300px] max-w-full mt-8 cursor-pointer hover:shadow-2xl hover:ring-2 ring-black">
-                    <div className="flex flex-col gap-2 mb-4">
-                        <h1 className="text-center font-extrabold text-3xl underline">Title: Coding</h1>
-                        <p>Description: All the tasks related to coding goes in here.</p>
-                    </div>
-                    <ul className="flex flex-col gap-1">
-                        <li className="items-center flex"><input type="checkbox" name="Checkbox" id="checkbox" />
-                            Doing DSA for 1 hour
-                            <button>✏️</button>
-                            <button>👍</button>
-                            <button>👎</button>
-                            <button>❌</button>
-                        </li>
-                        <li className="items-center flex"><input type="checkbox" name="Checkbox" id="checkbox" />
-                            Doing Development for 2 hour
-                            <button>✏️</button>
-                            <button>👍</button>
-                            <button>👎</button>
-                            <button>❌</button>
-                        </li>
-                        <li className="items-center flex"><input type="checkbox" name="Checkbox" id="checkbox" />
-                            Creating the dashboard layout
-                            <button>✏️</button>
-                            <button>👍</button>
-                            <button>👎</button>
-                            <button>❌</button>
-                        </li>
-                    </ul>
-                </div>
+                    
+                    <TaskCard />
 
-                 <div className="flex flex-col border-slate-900 border-2 rounded-3xl px-8 py-6 shadow-xl w-[300px] max-w-full mt-8 cursor-pointer hover:shadow-2xl hover:ring-2 ring-black">
-                    <div className="flex flex-col gap-2 mb-4">
-                        <h1 className="text-center font-extrabold text-3xl underline">Title: Coding</h1>
-                        <p>Description: All the tasks related to coding goes in here.</p>
-                    </div>
-                    <ul className="flex flex-col gap-1">
-                        <li className="items-center flex"><input type="checkbox" name="Checkbox" id="checkbox" />
-                            Doing DSA for 1 hour
-                            <button>✏️</button>
-                            <button>👍</button>
-                            <button>👎</button>
-                            <button>❌</button>
-                        </li>
-                        <li className="items-center flex"><input type="checkbox" name="Checkbox" id="checkbox" />
-                            Doing Development for 2 hour
-                            <button>✏️</button>
-                            <button>👍</button>
-                            <button>👎</button>
-                            <button>❌</button>
-                        </li>
-                        <li className="items-center flex"><input type="checkbox" name="Checkbox" id="checkbox" />
-                            Creating the dashboard layout
-                            <button>✏️</button>
-                            <button>👍</button>
-                            <button>👎</button>
-                            <button>❌</button>
-                        </li>
-                    </ul>
-                </div>
+                        {taskCard.map((card,index) =>
+                        <div className="flex flex-col border-slate-900 border-2 rounded-3xl px-8 py-6 shadow-xl w-[300px] max-w-full mt-8 cursor-pointer hover:shadow-2xl hover:ring-2 ring-black"> 
+                            <div key={index} className="flex flex-col gap-2 mb-4">
+                                <label className="text-center font-extrabold text-3xl underline">
+                                    Title: {card.title}<input type="text" name="Title" id="title" className="w-[100px]" onChange={handleTaskTitle}/>
+                                </label>
+                                <label>
+                                    Description: {card.description}<input type="text" name="Title" id="title" onChange={handleTaskDescription}/>
+                                </label>                                
+                            </div>
+                            <div>
+                                <input key={index} type="text" name="TaskName" id="taskname" className="border" value={task} />
+                                <button className="bg-slate-800 text-white p-2 font-extrabold rounded-lg shadow-lg hover:bg-slate-700 hover:shadow-2xl hover:shadow-slate-950 hover:scale-105 transform transition-transform duration-300" >+ Add Task</button>
+                            </div>
+                            <button >Set Card</button>
+                            <ol>
+                                <li>
+                                    {card.task}
+                                </li>
+                            </ol>
+                        </div>
+                        )}
 
-                 <div className="flex flex-col border-slate-900 border-2 rounded-3xl px-8 py-6 shadow-xl w-[300px] max-w-full mt-8 cursor-pointer hover:shadow-2xl hover:ring-2 ring-black">
-                    <div className="flex flex-col gap-2 mb-4">
-                        <h1 className="text-center font-extrabold text-3xl underline">Title: Coding</h1>
-                        <p>Description: All the tasks related to coding goes in here.</p>
-                    </div>
-                    <ul className="flex flex-col gap-1">
-                        <li className="items-center flex"><input type="checkbox" name="Checkbox" id="checkbox" />
-                            Doing DSA for 1 hour
-                            <button>✏️</button>
-                            <button>👍</button>
-                            <button>👎</button>
-                            <button>❌</button>
-                        </li>
-                        <li className="items-center flex"><input type="checkbox" name="Checkbox" id="checkbox" />
-                            Doing Development for 2 hour
-                            <button>✏️</button>
-                            <button>👍</button>
-                            <button>👎</button>
-                            <button>❌</button>
-                        </li>
-                        <li className="items-center flex"><input type="checkbox" name="Checkbox" id="checkbox" />
-                            Creating the dashboard layout
-                            <button>✏️</button>
-                            <button>👍</button>
-                            <button>👎</button>
-                            <button>❌</button>
-                        </li>
-                    </ul>
-                </div>
+
                 </div>
 
 
@@ -135,3 +103,8 @@ function DailyView({username}) {
     );
 }
 export default DailyView;
+
+/*
+                                <h1 className="text-center font-extrabold text-3xl underline">Title: {card.title}</h1>
+                                <p>{card.description}</p>
+*/ 
---
+++ src/components/tracker/TaskCard.jsx
@@ -0,0 +1,68 @@
+import React, {useState} from "react";
+
+function TaskCard() {
+
+    const [title, setTitle] = useState("");
+    const [description, setDescription] = useState("");
+    const [isCardSet, setIsCardSet] = useState(false);
+    const [taskInput, setTaskInput] = useState("");
+    const [task, setTask] = useState([]);
+
+    function handleTitle(event) {
+        setTitle(event.target.value);
+    }
+
+    function handleDescription(event) {
+        setDescription(event.target.value);
+    }
+
+    function handleTaskInput(event) {
+        setTaskInput(event.target.value);
+    }
+
+    function handleAddTask() {
+        if(taskInput.trim() === "") return;
+
+        setTask([...task, taskInput]);
+        setTaskInput("");
+    }
+
+    return (
+        <div className="flex flex-col border-slate-900 border-2 rounded-3xl px-8 py-6 shadow-xl w-[300px] max-w-full mt-8 cursor-pointer hover:shadow-2xl hover:ring-2 ring-black">
+            {
+                !isCardSet ? (
+                    <div className="flex flex-col gap-2 mb-4">
+                            <input type="text" placeholder="Enter the Title" value={title} onChange={handleTitle} 
+                            className="border border-black px-2 py-1 rounded" onKeyDown={(e) => {if(e.key === "Enter") setIsCardSet(true)} }/>
+                            <input type="text" placeholder="Description" value={description} onChange={handleDescription} 
+                            className="border border-black px-2 py-1 rounded" onKeyDown={(e) => {if(e.key === "Enter") setIsCardSet(true)} }/>
+                            <button className="border border-black px-4 py-2 rounded hover:bg-gray-100" onClick={() => {setIsCardSet(true)}}>
+                                Set Card
+                            </button>                                     
+                    </div>
+                ) : (
+                    <div className="flex flex-col gap-2 mb-4">
+                        <h1 className="text-center font-extrabold text-3xl underline">{title}</h1>
+                        <p className="text-gray-700">{description}</p>
+                        <div>
+                            <input type="text" placeholder="Add your tasks..." value={taskInput} onChange={handleTaskInput} /> 
+                            <button className="border border-black px-4 py-2 rounded hover:bg-gray-100" onClick={handleAddTask}>+</button>
+                        </div>
+                        <ol>
+                            {task.map((task,index) => 
+                                <li key={index}>
+                                    {task}
+                                </li>
+                            )}
+                        </ol>
+                            
+                                
+                        
+                        
+                    </div>
+                )
+            } 
+        </div>
+    );
+}
+export default TaskCard;
