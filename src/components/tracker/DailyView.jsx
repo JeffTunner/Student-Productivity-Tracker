19de5ad2755e561394828f7da5ae0dfcb7cc74f9
JeffTunner <abhinavkaushik05@gmail.com>
2025-08-07 22:10:42 +0530

    Day 10- Made the taskcard to dynamically add, added edit/delete button, improved more features

--- src/components/tracker/DailyView.jsx
+++ src/components/tracker/DailyView.jsx
@@ -21,33 +21,12 @@ function DailyView({username}) {
         day: 'numeric'
     });
 
-    const [taskCard, setTaskCard] = useState([]);
-    const [taskTitle, setTaskTitle] = useState("");
-    const [taskDescription, setTaskDescription] = useState("");
-    const [taskList, setTaskList] = useState([]);
-    const [task, setTask] = useState("");
+    const [cards, setCards] = useState([]);
 
-    function handleAddTaskCard() {
-
-        const newCard = {id: Date().now, title: taskTitle, description: taskDescription, list: []};
-        const newList = {name: task};
-
-        setTaskCard(tc => [...tc, newCard]);
-        setTaskList(tl => [...tl, newList]);
-        setTaskTitle("");
-        setTaskDescription("");
-        setTask("");
+    function handleAddCard() {
+        const newCard = {id: Date.now()};
+        setCards(c => [...c, newCard]);
     }
-
-    function handleTaskTitle(event) {
-        setTaskTitle(event.target.value);
-    }
-
-    function handleTaskDescription(event) {
-        setTaskDescription(event.target.value);
-    }
-
-    
 
     return (
     <div>
@@ -62,37 +41,15 @@ function DailyView({username}) {
                     <p>Click + to add one!</p>
                 </div>
                 <div className="mt-4">
-                    <button className="bg-slate-800 text-white p-2 font-extrabold rounded-lg shadow-lg hover:bg-slate-700 hover:shadow-2xl hover:shadow-slate-950 hover:scale-105 transform transition-transform duration-300" onClick={handleAddTaskCard}>+ Add Card</button>
+                    <button className="bg-slate-800 text-white p-2 font-extrabold rounded-lg shadow-lg hover:bg-slate-700 hover:shadow-2xl hover:shadow-slate-950 hover:scale-105 transform transition-transform duration-300" onClick={handleAddCard}>+ Add Card</button>
                 </div>
 
                 <div className="flex flex-wrap gap-8">
-                    
-                    <TaskCard />
-
-                        {taskCard.map((card,index) =>
-                        <div className="flex flex-col border-slate-900 border-2 rounded-3xl px-8 py-6 shadow-xl w-[300px] max-w-full mt-8 cursor-pointer hover:shadow-2xl hover:ring-2 ring-black"> 
-                            <div key={index} className="flex flex-col gap-2 mb-4">
-                                <label className="text-center font-extrabold text-3xl underline">
-                                    Title: {card.title}<input type="text" name="Title" id="title" className="w-[100px]" onChange={handleTaskTitle}/>
-                                </label>
-                                <label>
-                                    Description: {card.description}<input type="text" name="Title" id="title" onChange={handleTaskDescription}/>
-                                </label>                                
-                            </div>
-                            <div>
-                                <input key={index} type="text" name="TaskName" id="taskname" className="border" value={task} />
-                                <button className="bg-slate-800 text-white p-2 font-extrabold rounded-lg shadow-lg hover:bg-slate-700 hover:shadow-2xl hover:shadow-slate-950 hover:scale-105 transform transition-transform duration-300" >+ Add Task</button>
-                            </div>
-                            <button >Set Card</button>
-                            <ol>
-                                <li>
-                                    {card.task}
-                                </li>
-                            </ol>
-                        </div>
-                        )}
-
-
+                    {
+                        cards.map((card, index) => (
+                            <TaskCard key={card.id}/>
+                        ))
+                    }
                 </div>
 
 
--- src/components/tracker/TaskCard.jsx
+++ src/components/tracker/TaskCard.jsx
@@ -7,6 +7,7 @@ function TaskCard() {
     const [isCardSet, setIsCardSet] = useState(false);
     const [taskInput, setTaskInput] = useState("");
     const [task, setTask] = useState([]);
+    const [editText, setEditText] = useState("");
 
     function handleTitle(event) {
         setTitle(event.target.value);
@@ -21,11 +22,42 @@ function TaskCard() {
     }
 
     function handleAddTask() {
+        const newTask = {text: taskInput, editing: false};
         if(taskInput.trim() === "") return;
 
-        setTask([...task, taskInput]);
+        setTask(t => [...t, newTask]);
         setTaskInput("");
     }
+
+    function handleEditTask(index) {
+    const updatedTask = task.map((t, i) => {
+        if (i === index) {
+            setEditText(task[index].text);
+        return  { ...t, editing: true }; 
+        }
+        return t;
+    });
+    setTask(updatedTask);
+    }
+
+    function handleUpdateTask(index, newText) {
+        const updatedTask = task.map((t, i) => {
+        if (i === index) {
+            setEditText("");
+        return { ...t, text: newText, editing: false };
+        }
+        return t;
+        });
+        setTask(updatedTask);
+    }
+
+
+
+    function handleRemoveTask(index) {
+        const updatedTask = task.filter((_, i) => i !== index );
+        setTask(updatedTask);
+    }
+
 
     return (
         <div className="flex flex-col border-slate-900 border-2 rounded-3xl px-8 py-6 shadow-xl w-[300px] max-w-full mt-8 cursor-pointer hover:shadow-2xl hover:ring-2 ring-black">
@@ -38,7 +70,7 @@ function TaskCard() {
                             className="border border-black px-2 py-1 rounded" onKeyDown={(e) => {if(e.key === "Enter") setIsCardSet(true)} }/>
                             <button className="border border-black px-4 py-2 rounded hover:bg-gray-100" onClick={() => {setIsCardSet(true)}}>
                                 Set Card
-                            </button>                                     
+                            </button>                                  
                     </div>
                 ) : (
                     <div className="flex flex-col gap-2 mb-4">
@@ -49,17 +81,34 @@ function TaskCard() {
                             <button className="border border-black px-4 py-2 rounded hover:bg-gray-100" onClick={handleAddTask}>+</button>
                         </div>
                         <ol>
-                            {task.map((task,index) => 
+                            {task.map((task,index) => (
                                 <li key={index}>
-                                    {task}
+                                    {task.editing ? (
+                                        <input type="text" value={editText} className="border border-black px-2 py-1 rounded" 
+                                        onChange={(e) => setEditText(e.target.value)} 
+                                        onKeyDown={(e) => {if(e.key === "Enter"){ handleUpdateTask(index, editText)}    
+                                            if (e.key === "Escape") {
+                                            const updatedTask = task.map((t, i) =>
+                                            i === index ? { ...t, editing: false } : t
+                                            );
+                                            setTask(updatedTask);
+                                            setEditText("");
+                                        } }}/>
+                                    ) : (
+                                        <>
+                                        <input type="checkbox" />
+                                        <span id="text" className="ml-2">{task.text}</span>
+                                        <button className="border border-black p-2 ml-2" onClick={() => handleEditTask(index)}>✏️</button>
+                                        <button className="border border-black p-2 ml-2" onClick={() => handleRemoveTask(index)}>❌</button>
+                                        </>
+                                    )
+                                    }
                                 </li>
+                            )
                             )}
                         </ol>
-                            
-                                
-                        
-                        
-                    </div>
+                                    
+                   </div>
                 )
             } 
         </div>
